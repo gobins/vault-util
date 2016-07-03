c9ce9d629a0981b576e86d8aeeb30349d2657880
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-07-03 20:26:23 +1000

    Adding getClient method to retrieve a Vault client

---
+++ client.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/hashicorp/vault/api"
+)
+
+func getClient(vaultAddress string) *api.Client {
+	c := api.Config{Address: vaultAddress}
+	response := c.ReadEnvironment()
+	if response != nil {
+		log.Error("Error reading local env: ", response.Error())
+	}
+	client, err := api.NewClient(&c)
+	if err != nil {
+		log.Error("Error creating Vault Client: ", err.Error())
+	}
+	return client
+}
