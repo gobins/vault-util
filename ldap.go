9174b859367f670ca3f8f0299af325a812a04535
gobins <gobinsougrakpam@gmail.com>
2016-09-08 13:47:28 +1000

    Adding GetGroupPolicy call

--- ldap.go
+++ ldap.go
@@ -4,8 +4,16 @@ import (
 	vaultapi "github.com/hashicorp/vault/api"
 )
 
-func GetGroups(c *vaultapi.Client) (*api.Secret, error) {
+func GetGroups(c *vaultapi.Client) (*vaultapi.Secret, error) {
 	resp, err := c.Logical().List("auth/ldap/groups")
+	if err != nil {
+		return resp, err
+	}
+	return resp, err
+}
+
+func GetGroupPolicy(c *vaultapi.Client, group_name string) (*vaultapi.Secret, error) {
+	resp, err := c.Logical().Read("auth/ldap/groups/" + group_name)
 	if err != nil {
 		return resp, err
 	}
